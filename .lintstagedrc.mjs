8593b789a671c58d6e3052895a185d6f8e18a5b8
安忆 <i@anyi.in>
2024-06-04 01:47:41 +0800

    build: enable style lint for SCSS files

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -7,8 +7,8 @@ const buildEslintCommand = (filenames) =>
 	`next lint --fix --file ${filenames.map((f) => relative(process.cwd(), f)).join(' --file ')}`;
 
 export default {
-	'*.css': ['prettier --write', 'stylelint --allow-empty-input --fix'],
+	'*.scss': ['prettier --write', 'stylelint --allow-empty-input --fix'],
 	'*.{cjs,mjs,js,jsx,ts,tsx}': ['prettier --write', buildEslintCommand],
 	'*.svg': 'svgo --config=svgo.config.cjs --quiet --input',
-	'!*.{css,cjs,mjs,js,jsx,ts,tsx,svg}': 'prettier --ignore-unknown --write',
+	'!*.{scss,cjs,mjs,js,jsx,ts,tsx,svg}': 'prettier --ignore-unknown --write',
 };
