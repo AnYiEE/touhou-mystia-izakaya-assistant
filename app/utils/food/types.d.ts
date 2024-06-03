d1ccd0b7553b5232a176dca14d7a6126307a96ab
安忆 <i@anyi.in>
2024-06-04 02:07:22 +0800

    fix(utils/food): generic constraints of parameters

--- app/utils/food/types.d.ts
+++ app/utils/food/types.d.ts
@@ -1,6 +1,11 @@
 import type {IFoodBase} from '@/data/types';
+import type {BeverageNames} from '@/data/beverages';
+import type {IngredientNames} from '@/data/ingredients';
+import type {RecipeNames} from '@/data/recipes';
 
-interface IFood<T = string> extends Omit<IFoodBase, 'from'> {
+type FoodNames = BeverageNames | IngredientNames | RecipeNames;
+
+interface IFood<T extends FoodNames = FoodNames> extends Omit<IFoodBase, 'from'> {
 	name: T;
 }
 
