07648cc4d46c4bb614f5bea95affec552a2fa2df
安忆 <i@anyi.in>
2024-06-05 16:54:45 +0800

    feat: add basic page layout

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import {type Metadata} from 'next';
+
+import {Button, Link} from '@nextui-org/react';
+
+export const metadata: Metadata = {
+	title: 'Oops!',
+};
+
+export default function NotFound() {
+	return (
+		<div className="flex h-full items-center justify-center space-x-4">
+			<h1 className="text-6xl font-bold">404</h1>
+			<div className="h-12 w-px bg-foreground" />
+			<p className="hidden text-xl md:inline">找不到您所请求的资源</p>
+			<Button as={Link} color="primary" href="/" size="sm" variant="faded">
+				返回首页
+			</Button>
+		</div>
+	);
+}
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,24 @@
 'use client';
 
+import {type ReactNode} from 'react';
+import {useRouter} from 'next/navigation';
+
 import {NextUIProvider} from '@nextui-org/react';
 import {ThemeProvider as NextThemesProvider} from 'next-themes';
+import type {ThemeProviderProps} from 'next-themes/dist/types';
 
-export function Providers({children}: {children: React.ReactNode}) {
+interface IProvidersProps {
+	children: ReactNode;
+	locale: string;
+	themeProps?: Omit<ThemeProviderProps, 'children'> & {children?: ReactNode};
+}
+
+export function Providers({children, locale, themeProps}: IProvidersProps) {
+	const router = useRouter();
+
 	return (
-		<NextUIProvider>
-			<NextThemesProvider attribute="class" defaultTheme="light">
-				{children}
-			</NextThemesProvider>
+		<NextUIProvider locale={locale} navigate={router.push}>
+			<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
 		</NextUIProvider>
 	);
 }
