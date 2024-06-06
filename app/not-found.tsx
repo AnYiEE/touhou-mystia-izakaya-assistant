d936c49962e90a7fbca40e4fd3a6a604663d2e0c
安忆 <i@anyi.in>
2024-06-06 18:05:22 +0800

    refactor(layouts/not-found): replace `div` with `Divider` components

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 import {type Metadata} from 'next';
 
-import {Button, Link} from '@nextui-org/react';
+import {Button, Divider, Link} from '@nextui-org/react';
 
 export const metadata: Metadata = {
 	title: 'Oops!',
@@ -10,7 +10,7 @@ export default function NotFound() {
 	return (
 		<div className="flex h-full items-center justify-center space-x-4">
 			<h1 className="text-6xl font-bold">404</h1>
-			<div className="h-12 w-px bg-foreground" />
+			<Divider orientation="vertical" className="h-12" />
 			<p className="hidden text-xl md:inline">找不到您所请求的资源</p>
 			<Button as={Link} color="primary" href="/" size="sm" variant="faded">
 				返回首页
