cdd0ea37eae1c06bb9e47036d5714670475b59ba
安忆 <i@anyi.in>
2024-06-06 12:48:27 +0800

    feat(components): add `ItemCard`

---
+++ app/components/itemCard.tsx
@@ -0,0 +1,29 @@
+import {type ReactNode} from 'react';
+
+import {Card, type CardProps} from '@nextui-org/react';
+
+interface IProps {
+	name: string;
+	description: string;
+	image: ReactNode;
+	isDisabled: CardProps['isDisabled'];
+	isHoverable: CardProps['isHoverable'];
+	isPressable: CardProps['isPressable'];
+	onPress: CardProps['onPress'];
+}
+
+export default function ItemCard(props: Partial<IProps>) {
+	const {name, description, image, onPress, ...cardProps} = props;
+
+	return (
+		<Card shadow="sm" className="w-full" onPress={onPress ?? (() => {})} {...cardProps}>
+			<div className="flex items-center">
+				<div className="m-1 flex rounded-xl shadow-[inset_0_0_2px] shadow-foreground-400">{image}</div>
+				<div className="mx-1 inline-flex flex-col text-left">
+					<p className="text-small font-medium">{name ?? ''}</p>
+					<p className="mt-1 text-xs font-light text-default-500">{description ?? ''}</p>
+				</div>
+			</div>
+		</Card>
+	);
+}
