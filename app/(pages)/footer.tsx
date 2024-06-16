e1e66b7e6e9eec9c66da960d462adaeadaef89a0
安忆 <i@anyi.in>
2024-06-16 08:52:13 +0800

    refactor(components/Footer): remove unnecessary hooks

--- app/(pages)/footer.tsx
+++ app/(pages)/footer.tsx
@@ -1,10 +1,4 @@
-'use client';
-
 import {type PropsWithChildren, type ReactNode} from 'react';
-import {useTheme} from 'next-themes';
-import clsx from 'clsx';
-
-import {useMounted} from '@/hooks';
 
 import {Link, Tooltip} from '@nextui-org/react';
 
@@ -14,14 +8,12 @@ interface IFooterLinkProps {
 	href: string;
 	tipContent: ReactNode;
 	isExternal: boolean;
-	isLight: boolean;
 	isShowIcon: boolean;
 }
 
 function FooterLink({
 	href = '#',
 	tipContent = '',
-	isLight = true,
 	isExternal = true,
 	isShowIcon = false,
 	children,
@@ -32,9 +24,7 @@ function FooterLink({
 				isExternal={isExternal}
 				showAnchorIcon={isShowIcon}
 				href={href}
-				className={clsx('text-xs', isLight ? 'opacity-30' : 'opacity-40', {
-					'text-warning-400': !isLight,
-				})}
+				className={'text-xs opacity-30 dark:text-warning-400 dark:opacity-40'}
 			>
 				{children}
 			</Link>
@@ -43,32 +33,14 @@ function FooterLink({
 }
 
 export default function Footer() {
-	const isMounted = useMounted();
-	const {theme} = useTheme();
-
-	if (!isMounted) {
-		return null;
-	}
-
-	const isLight = ['light', 'system'].includes(theme ?? '');
-
 	return (
 		<footer className="mx-auto flex max-w-[95%] items-center justify-center pb-3 md:max-w-full">
 			<p className="text-center text-xs text-default-300 dark:text-default-400">
-				<FooterLink
-					isLight={isLight}
-					href={siteConfig.links.github.href}
-					tipContent={siteConfig.links.github.label}
-				>
+				<FooterLink href={siteConfig.links.github.href} tipContent={siteConfig.links.github.label}>
 					{siteConfig.shortName}
 				</FooterLink>
 				内所涉及的公司名称、商标、产品等均为其各自所有者的资产，仅供识别。游戏素材版权均归
-				<FooterLink
-					isLight={isLight}
-					isShowIcon
-					href={siteConfig.links.steam.href}
-					tipContent={siteConfig.links.steam.label}
-				>
+				<FooterLink isShowIcon href={siteConfig.links.steam.href} tipContent={siteConfig.links.steam.label}>
 					原作者
 				</FooterLink>
 				所有
