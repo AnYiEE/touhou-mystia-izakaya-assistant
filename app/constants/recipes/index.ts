5fbeb4aad4dccef86fc106853523dbcff32ca0d9
安忆 <i@anyi.in>
2024-06-16 17:53:14 +0800

    chore: update tag color constant data

--- app/constants/recipes/index.ts
+++ app/constants/recipes/index.ts
@@ -9,6 +9,6 @@ export const RECIPE_TAG_STYLE = {
 	negative: {
 		backgroundColor: '#5d453a',
 		borderColor: '#000000',
-		color: '#e40d0d',
+		color: '#e6b4a6', // 游戏里的#e40d0d在这里对比度太低
 	},
 } as const satisfies ITagStyle;
