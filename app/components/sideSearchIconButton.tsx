531a9d9cf31bec142abb20cdd4cf68f9407093ac
安忆 <i@anyi.in>
2024-06-17 16:25:51 +0800

    refactor(components/SideSearchIconButton): integrate `Autocomplete` with `Popover` Replace `Input` with `Autocomplete` component to enhance the side search button functionality, allowing for item selection from a list

--- app/components/sideSearchIconButton.tsx
+++ app/components/sideSearchIconButton.tsx
@@ -1,24 +1,27 @@
 import {forwardRef, type Dispatch, type SetStateAction} from 'react';
 
-import {Input, Popover, PopoverContent, PopoverTrigger} from '@nextui-org/react';
+import {Autocomplete, AutocompleteItem, Popover, PopoverContent, PopoverTrigger} from '@nextui-org/react';
 import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 
 import FontAwesomeIconButton, {type IFontAwesomeIconButtonProps} from '@/components/fontAwesomeIconButton';
 
 interface IProps extends Omit<IFontAwesomeIconButtonProps, 'aria-label' | 'color' | 'icon' | 'variant' | 'onPress'> {
-	searchValue: string;
-	setSearchValue: Dispatch<SetStateAction<string>>;
+	searchItems: {
+		value: string;
+	}[];
+	searchValue: string | null;
+	setSearchValue: Dispatch<SetStateAction<IProps['searchValue']>>;
 }
 
 export default forwardRef<HTMLDivElement | null, IProps>(function SideSearchIconButton(
-	{searchValue, setSearchValue, ...props},
+	{searchItems, searchValue, setSearchValue, ...props},
 	ref
 ) {
 	return (
-		<Popover backdrop="opaque" placement="left" showArrow ref={ref}>
+		<Popover backdrop="opaque" placement="left" showArrow shouldCloseOnInteractOutside={() => true} ref={ref}>
 			<PopoverTrigger>
 				<FontAwesomeIconButton
-					color={searchValue.length ? 'warning' : 'primary'}
+					color={searchValue ? 'warning' : 'primary'}
 					icon={faMagnifyingGlass}
 					variant="shadow"
 					aria-label="搜索"
@@ -26,13 +29,18 @@ export default forwardRef<HTMLDivElement | null, IProps>(function SideSearchIcon
 				/>
 			</PopoverTrigger>
 			<PopoverContent>
-				<Input
-					isClearable
+				<Autocomplete
+					allowsCustomValue
 					variant="faded"
 					label="请输入您想要搜索的名称"
-					value={searchValue}
-					onValueChange={setSearchValue}
-				/>
+					defaultItems={searchItems}
+					selectedKey={searchValue}
+					onSelectionChange={(key) => {
+						setSearchValue(key as string);
+					}}
+				>
+					{(item) => <AutocompleteItem key={item.value}>{item.value}</AutocompleteItem>}
+				</Autocomplete>
 			</PopoverContent>
 		</Popover>
 	);
