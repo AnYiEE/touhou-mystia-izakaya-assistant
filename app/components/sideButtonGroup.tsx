c8da5d6192f876da612ba8615a161244dfb7986d
安忆 <i@anyi.in>
2024-06-25 14:46:49 +0800

    refactor(components/SideButtonGroup): add `className` prop and use `clsx` for styling Extend the SideButtonGroup component to accept a className prop. This enhances the component's customizability by allowing external classes to be applied. The internal div now uses clsx to manage the concatenation of dynamic and static classes.

--- app/components/sideButtonGroup.tsx
+++ app/components/sideButtonGroup.tsx
@@ -1,12 +1,15 @@
 import {forwardRef, memo, type PropsWithChildren} from 'react';
+import clsx from 'clsx';
 
-interface IProps {}
+interface IProps {
+	className?: string;
+}
 
 export default memo(
-	forwardRef<HTMLDivElement | null, PropsWithChildren<IProps>>(function SideButtonGroup({children}, ref) {
+	forwardRef<HTMLDivElement | null, PropsWithChildren<IProps>>(function SideButtonGroup({className, children}, ref) {
 		return (
 			<div className="absolute" ref={ref}>
-				<div className="fixed bottom-6 right-6 z-20 md:bottom-[calc(50%-4rem)]">
+				<div className={clsx('fixed bottom-6 right-6 z-20 w-min md:bottom-[calc(50%-4rem)]', className)}>
 					<div className="flex flex-col gap-3">{children}</div>
 				</div>
 			</div>
