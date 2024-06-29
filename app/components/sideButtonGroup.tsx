c689ec2df72d7b5d5cb9e769e408c2275b4ae987
安忆 <i@anyi.in>
2024-06-29 14:53:43 +0800

    refactor(components/SideButtonGroup): adjust bottom position calculation for responsiveness

--- app/components/sideButtonGroup.tsx
+++ app/components/sideButtonGroup.tsx
@@ -9,7 +9,7 @@ export default memo(
 	forwardRef<HTMLDivElement | null, PropsWithChildren<IProps>>(function SideButtonGroup({className, children}, ref) {
 		return (
 			<div className="absolute" ref={ref}>
-				<div className={clsx('fixed bottom-6 right-6 z-20 w-min md:bottom-[calc(50%-4rem)]', className)}>
+				<div className={clsx('fixed bottom-6 right-6 z-20 w-min md:bottom-[calc(50%-4.5rem)]', className)}>
 					<div className="flex flex-col gap-3">{children}</div>
 				</div>
 			</div>
