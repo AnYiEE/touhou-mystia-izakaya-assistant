f808b9905546d32c108133bca8cfdaf4e6812811
安忆 <i@anyi.in>
2024-07-09 20:12:48 +0800

    refactor(layouts.loading): adjust loading spinner size and position Update the loading.png image to a new version. Update the loading spinner's CSS to set explicit height and width, and modify the surrounding divs for better centering and scaling.

--- app/loading.tsx
+++ app/loading.tsx
@@ -10,12 +10,11 @@ interface IProps {
 export default memo(
 	forwardRef<HTMLDivElement | null, IProps>(function Loading({content = '少女料理中...'}, ref) {
 		return (
-			<div
-				className="flex h-full w-full select-none flex-col items-center justify-center text-sm tracking-widest"
-				ref={ref}
-			>
-				<span title={content} className={clsx(styles['logo'], 'h-16 w-16 animate-bounce')} />
-				<p className="mt-1">{content}</p>
+			<div className="flex h-full w-full select-none flex-col items-center justify-center" ref={ref}>
+				<div className="mb-3 scale-[175%]">
+					<span title={content} className={clsx(styles['logo'], 'inline-block animate-bounce')} />
+				</div>
+				<p className="text-sm tracking-widest">{content}</p>
 			</div>
 		);
 	})
