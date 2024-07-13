f10d2642624566463738a2914bf21512579d7aa6
安忆 <i@anyi.in>
2024-07-14 07:31:55 +0800

    refactor(layouts/index): add Xiaohongshu QR code for community engagement Introduce a QR code image and updated content for users to join the Xiaohongshu group.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import {memo} from 'react';
 
+import {Image} from '@nextui-org/react';
+
 import Loading from '@/loading';
 
 import {siteConfig} from '@/configs';
@@ -7,5 +9,28 @@ import {siteConfig} from '@/configs';
 const {shortName} = siteConfig;
 
 export default memo(function Home() {
-	return <Loading content={`欢迎使用${shortName}`} />;
+	return (
+		<div className="flex h-full flex-col items-center justify-center gap-4 md:gap-8 lg:flex-row lg:gap-16">
+			<Loading
+				content={
+					<>
+						分享经验、交流心得
+						<br />
+						提出建议、反馈问题
+						<br />
+						皆可扫描二维码加群
+					</>
+				}
+				title={`欢迎使用${shortName}`}
+				className="h-max w-max"
+			/>
+			<Image
+				isBlurred
+				alt="小红书加群二维码"
+				draggable={false}
+				src="/assets/Xiaohongshu.png"
+				className="h-96 select-none dark:brightness-75 dark:contrast-125"
+			/>
+		</div>
+	);
 });
