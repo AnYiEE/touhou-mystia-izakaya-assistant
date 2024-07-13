e8501ed5bdfa26a3f4b9dbcaab990b0205a4ab45
安忆 <i@anyi.in>
2024-07-14 07:25:15 +0800

    refactor(layouts/loading): extend `Loading` component props and improve UI flexibility Extend the Loading component's props to include className and title.

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,18 +1,26 @@
-import {forwardRef, memo} from 'react';
+import {type HTMLAttributes, forwardRef, memo} from 'react';
 import clsx from 'clsx/lite';
 
 import styles from './loading.module.scss';
 
-interface IProps {
-	content?: string;
+interface IProps
+	extends Pick<HTMLAttributes<HTMLDivElement>, 'className'>,
+		Pick<HTMLAttributes<HTMLSpanElement>, 'title'> {
+	content?: ReactNodeWithoutBoolean;
 }
 
 export default memo(
-	forwardRef<HTMLDivElement | null, IProps>(function Loading({content = '少女料理中...'}, ref) {
+	forwardRef<HTMLDivElement | null, IProps>(function Loading({className, content = '少女料理中...', title}, ref) {
 		return (
-			<div className="flex h-full w-full select-none flex-col items-center justify-center" ref={ref}>
+			<div
+				className={clsx('flex h-full w-full select-none flex-col items-center justify-center', className)}
+				ref={ref}
+			>
 				<div className="mb-3 scale-[175%]">
-					<span title={content} className={clsx(styles['logo'], 'inline-block animate-bounce')} />
+					<span
+						title={title ?? (content as string)}
+						className={clsx(styles['logo'], 'inline-block animate-bounce')}
+					/>
 				</div>
 				<p className="text-sm tracking-widest">{content}</p>
 			</div>
