56ddde464ab96fe9d26547802635fec08b284028
安忆 <i@anyi.in>
2024-07-14 18:19:57 +0800

    refactor(layouts/index): add QQ QR code and add update UI - Add QQ QR code and link. - Create a new Xiaohongshu QR code component for community engagement. - Update the UI to integrate the QR code component and improve the overall layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import {memo} from 'react';
 
-import {Image} from '@nextui-org/react';
+import {Button, Divider, Image, Link, Tooltip} from '@nextui-org/react';
+import {faQq} from '@fortawesome/free-brands-svg-icons';
 
 import Loading from '@/loading';
+import FontAwesomeIconLink from '@/components/fontAwesomeIconLink';
+import Xiaohongshu from '@/components/xiaohongshu';
 
 import {siteConfig} from '@/configs';
 
@@ -10,27 +13,69 @@ const {shortName} = siteConfig;
 
 export default memo(function Home() {
 	return (
-		<div className="flex h-full flex-col items-center justify-center gap-4 md:gap-8 lg:flex-row lg:gap-16">
-			<Loading
-				content={
-					<>
-						分享经验、交流心得
-						<br />
-						提出建议、反馈问题
-						<br />
-						皆可扫描二维码加群
-					</>
-				}
-				title={`欢迎使用${shortName}`}
-				className="h-max w-max"
-			/>
-			<Image
-				isBlurred
-				alt="小红书加群二维码"
-				draggable={false}
-				src="/assets/Xiaohongshu.png"
-				className="h-96 select-none dark:brightness-75 dark:contrast-125"
-			/>
+		<div className="flex h-full flex-col items-center justify-center gap-4 md:flex-row md:gap-8">
+			<Loading content={`欢迎使用${shortName}`} className="h-max w-max" />
+			<Divider className="w-12 md:hidden" />
+			<Divider orientation="vertical" className="hidden h-12 md:block" />
+			<div className="flex gap-4">
+				<Tooltip
+					showArrow
+					content={
+						<div className="flex select-none flex-col items-center">
+							<Image
+								isBlurred
+								alt="QQ加群二维码"
+								draggable={false}
+								src="/assets/QQ.png"
+								className="h-32 dark:invert"
+							/>
+							<p className="text-xs">
+								分享经验、交流心得
+								<br />
+								提出建议、反馈问题
+							</p>
+						</div>
+					}
+				>
+					<FontAwesomeIconLink
+						isExternal
+						icon={faQq}
+						href="https://qm.qq.com/q/Zham0MdxyA"
+						title="点击加入QQ群"
+						className="text-xl"
+					/>
+				</Tooltip>
+				<Tooltip
+					showArrow
+					content={
+						<div className="flex select-none flex-col items-center">
+							<Image
+								isBlurred
+								alt="小红书加群二维码"
+								draggable={false}
+								src="/assets/Xiaohongshu.png"
+								className="h-32 dark:invert"
+							/>
+							<p className="text-xs">
+								分享经验、交流心得
+								<br />
+								提出建议、反馈问题
+							</p>
+						</div>
+					}
+				>
+					<Button
+						as={Link}
+						isExternal
+						isIconOnly
+						href="https://www.xiaohongshu.com/sns/invitation/group-chat?groupId=136956731996869234&token=xMbzu2IneK_mgPAGyJxxpYKyeP-zm1PsHZlLjIcS1uXycTLrNNVeaQoPbTJuXF0pMpFI1jYfuoDcRJQ1JhHwwPU3mAO9sg7JQchjlQGp6bs"
+						title="点击加入小红书群"
+						className="h-5"
+					>
+						<Xiaohongshu className="fill-transparent" />
+					</Button>
+				</Tooltip>
+			</div>
 		</div>
 	);
 });
