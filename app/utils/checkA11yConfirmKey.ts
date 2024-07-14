9de5cae014c3888da7766dab4ed674c6fb549447
安忆 <i@anyi.in>
2024-07-15 03:31:10 +0800

    perf(utils/checkA11yConfirmKey): replace includes with direct comparison for event type checking

--- app/utils/checkA11yConfirmKey.ts
+++ app/utils/checkA11yConfirmKey.ts
@@ -1,10 +1,15 @@
 import {type KeyboardEvent as ReactKeyboardEvent, MouseEvent as ReactMouseEvent} from 'react';
+
 export function checkA11yConfirmKey(event: ReactKeyboardEvent | ReactMouseEvent | KeyboardEvent | MouseEvent) {
-	if (['click', 'keydown'].includes(event.type)) {
-		if (event.type === 'keydown') {
-			return ['Enter', ' '].includes((event as KeyboardEvent).key);
+	const {type} = event;
+
+	if (type === 'click' || type === 'keydown') {
+		if (type === 'keydown') {
+			return (event as KeyboardEvent).key === 'Enter' || (event as KeyboardEvent).key === ' ';
 		}
+
 		return true;
 	}
+
 	return false;
 }
