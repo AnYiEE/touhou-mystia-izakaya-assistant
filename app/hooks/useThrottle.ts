b2570041715f5ae03e58a0020f58e9aa3592fe1a
安忆 <i@anyi.in>
2024-07-17 01:40:56 +0800

    fix(hooks/useThrottle): ensure immediate update on initial render This fix updates the `throttledValue` immediately on the first render, ensuring that the latest value is reflected without delay.

--- app/hooks/useThrottle.ts
+++ app/hooks/useThrottle.ts
@@ -2,9 +2,21 @@ import {useEffect, useRef, useState} from 'react';
 
 export function useThrottle<T>(value: T, limit: number = 1000) {
 	const [throttledValue, setThrottledValue] = useState(value);
-	const lastRan = useRef(Date.now());
+	const lastRan = useRef(0);
+	const initialRun = useRef(0);
 
 	useEffect(() => {
+		if (initialRun.current < 1) {
+			initialRun.current++;
+			return;
+		}
+		if (initialRun.current === 1) {
+			setThrottledValue(value);
+			lastRan.current = Date.now();
+			initialRun.current++;
+			return;
+		}
+
 		const handler = setTimeout(
 			() => {
 				if (Date.now() - lastRan.current >= limit) {
