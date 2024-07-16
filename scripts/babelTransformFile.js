e58742ca6d471438b9406c1ff81f84ff07997f89
安忆 <i@anyi.in>
2024-07-16 20:19:11 +0800

    build: add Babel transform for operators and syntaxes

---
+++ scripts/babelTransformFile.js
@@ -0,0 +1,36 @@
+// @ts-check
+/* eslint-disable @typescript-eslint/no-var-requires, unicorn/prefer-module */
+/**
+ * @file Transform all JavaScript operators and syntaxes, **not methods**.
+ */
+const {globSync} = require('glob');
+const {transformFile} = require('@babel/core');
+const {writeFileSync} = require('node:fs');
+
+globSync('out/**/*.js').forEach((filePath) => {
+	transformFile(
+		filePath,
+		{
+			comments: false,
+			compact: true,
+			presets: [
+				[
+					'@babel/preset-env',
+					{
+						modules: false,
+					},
+				],
+			],
+			/** @see {@link https://nextjs.org/docs/architecture/supported-browsers} */
+			targets: ['chrome 64', 'edge 79', 'firefox 67', 'opera 51', 'safari 12'],
+		},
+		(error, result) => {
+			if (error || !result || !result.code) {
+				console.error(`Error transforming file: ${filePath}`, error);
+				return;
+			}
+
+			writeFileSync(filePath, result.code);
+		}
+	);
+});
