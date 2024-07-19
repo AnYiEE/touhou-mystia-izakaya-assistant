2dde69a8a8aa70d46e21d98d9a43a4d97c3054ad
安忆 <i@anyi.in>
2024-07-19 20:23:01 +0800

    perf(utils/pinyinSort): add caching for pinyin conversions Introduce a caching mechanism to store pinyin conversions in order to reduce the computational overhead when sorting elements by pinyin. This optimization enhances the performance of pinyin sorting operations by avoiding repeated conversions.

--- app/utils/pinyinSort.ts
+++ app/utils/pinyinSort.ts
@@ -23,23 +23,37 @@ function removeTone(pinyin: string) {
 	return pinyin.replace(/\d/u, '');
 }
 
+const pinyinCache = new Map<string, string[]>();
+
 export function pinyinSort(a: TTarget, b: TTarget) {
 	a = checkValueObject(a) ? a.value : a;
 	b = checkValueObject(b) ? b.value : b;
 
 	if (isString(a)) {
-		a = pinyinPro(a, {
-			toneType: 'num',
-			type: 'array',
-			v: true,
-		});
+		if (pinyinCache.has(a)) {
+			a = pinyinCache.get(a);
+		} else {
+			const pinyinA = pinyinPro(a, {
+				toneType: 'num',
+				type: 'array',
+				v: true,
+			});
+			pinyinCache.set(a, pinyinA);
+			a = pinyinA;
+		}
 	}
 	if (isString(b)) {
-		b = pinyinPro(b, {
-			toneType: 'num',
-			type: 'array',
-			v: true,
-		});
+		if (pinyinCache.has(b)) {
+			b = pinyinCache.get(b);
+		} else {
+			const pinyinB = pinyinPro(b, {
+				toneType: 'num',
+				type: 'array',
+				v: true,
+			});
+			pinyinCache.set(b, pinyinB);
+			b = pinyinB;
+		}
 	}
 
 	const length = Math.min(a.length, b.length);
