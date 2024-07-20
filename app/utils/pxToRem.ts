6c9e4190f79968d144bf8359ce721163274861e2
安忆 <i@anyi.in>
2024-07-20 17:26:22 +0800

    refactor(utils): simplify type definitions and use `TResult` for `pxToRem` and `remToPx` Refactor the `pxToRem` and `remToPx` utility functions to use the new `TResult` type, which simplifies the return type definition. This change streamlines the function signatures and improves readability of the type system within these utilities.

--- app/utils/pxToRem.ts
+++ app/utils/pxToRem.ts
@@ -1,16 +1,14 @@
 import {isNil, isNumber} from 'lodash';
 
 type TPxString = `${string}px`;
+type TResult<T> = T extends TPxString ? number : T;
 
-export function pxToRem<T extends number | TPxString | null | undefined>(
-	px: T,
-	rootFontSize = 16
-): T extends TPxString ? number : T {
+export function pxToRem<T extends number | TPxString | null | undefined>(px: T, rootFontSize = 16): TResult<T> {
 	if (isNil(px)) {
-		return px as T extends TPxString ? number : T;
+		return px as TResult<T>;
 	}
 
 	const numericValue = isNumber(px) ? px : Number(px.slice(0, -2));
 
-	return (numericValue / rootFontSize) as T extends TPxString ? number : T;
+	return (numericValue / rootFontSize) as TResult<T>;
 }
--- app/utils/remToPx.ts
+++ app/utils/remToPx.ts
@@ -1,16 +1,14 @@
 import {isNil, isNumber} from 'lodash';
 
 type TRemString = `${string}rem`;
+type TResult<T> = T extends TRemString ? number : T;
 
-export function remToPx<T extends number | TRemString | null | undefined>(
-	rem: T,
-	rootFontSize = 16
-): T extends TRemString ? number : T {
+export function remToPx<T extends number | TRemString | null | undefined>(rem: T, rootFontSize = 16): TResult<T> {
 	if (isNil(rem)) {
-		return rem as T extends TRemString ? number : T;
+		return rem as TResult<T>;
 	}
 
 	const numericValue = isNumber(rem) ? rem : Number(rem.slice(0, -3));
 
-	return (numericValue * rootFontSize) as T extends TRemString ? number : T;
+	return (numericValue * rootFontSize) as TResult<T>;
 }
