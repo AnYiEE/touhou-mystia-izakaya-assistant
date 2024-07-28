0aec1402ca3eb23cff16fa7e1f408e1d22b76229
安忆 <i@anyi.in>
2024-07-29 01:10:44 +0800

    fix(utils/processPinyin): remove invalid characters from pinyin input Refactor the `processPinyin` function to filter out non-word characters after removing tone numbers from pinyin strings. This ensures that only valid pinyin characters are processed.

--- app/utils/processPinyin.ts
+++ app/utils/processPinyin.ts
@@ -1,5 +1,7 @@
 export function processPinyin(pinyin: string[]) {
-	const pinyinWithoutTone = pinyin.map((string) => string.replace(/\d/u, ''));
+	const pinyinWithoutTone = pinyin
+		.map((string) => string.replace(/\d/u, '').toLowerCase())
+		.filter((string) => /\w/u.test(string));
 	const pinyinFirstLetters = pinyinWithoutTone.map((string) => string.charAt(0)).join('');
 
 	return {
