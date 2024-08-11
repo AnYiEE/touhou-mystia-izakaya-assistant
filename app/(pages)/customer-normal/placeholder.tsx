466e4c898113cbcdf651936b5537fb9e37f65712
安忆 <i@anyi.in>
2024-08-12 02:16:48 +0800

    refactor(pages/customer-normal): reuse components from customer-rare Export the `Placeholder` and `TagGroup` components from the customer-rare module to be used within the customer-normal page. This change removes the need for duplication and takes advantage of existing components that are already optimized and tested.

--- app/(pages)/customer-normal/placeholder.tsx
+++ app/(pages)/customer-normal/placeholder.tsx
@@ -1,17 +1,1 @@
-import {type HTMLAttributes, type PropsWithChildren, forwardRef, memo} from 'react';
-import {twMerge} from 'tailwind-merge';
-
-interface IProps extends Pick<HTMLAttributes<HTMLDivElement>, 'className'> {}
-
-export default memo(
-	forwardRef<HTMLDivElement | null, PropsWithChildren<IProps>>(function Placeholder({className, children}, ref) {
-		return (
-			<div
-				className={twMerge('my-auto select-none text-center font-semibold text-default-300', className)}
-				ref={ref}
-			>
-				{children}
-			</div>
-		);
-	})
-);
+export {default} from '@/(pages)/customer-rare/placeholder';
--- app/(pages)/customer-normal/tagGroup.tsx
+++ app/(pages)/customer-normal/tagGroup.tsx
@@ -1,14 +1,1 @@
-import {type HTMLAttributes, type PropsWithChildren, forwardRef, memo} from 'react';
-import {twMerge} from 'tailwind-merge';
-
-interface IProps extends Pick<HTMLAttributes<HTMLDivElement>, 'className'> {}
-
-export default memo(
-	forwardRef<HTMLDivElement | null, PropsWithChildren<IProps>>(function TagGroup({className, children}, ref) {
-		return (
-			<div className={twMerge('flex flex-wrap gap-2', className)} ref={ref}>
-				{children}
-			</div>
-		);
-	})
-);
+export {default} from '@/(pages)/customer-rare/tagGroup';
