c77e538da40c6cf29257f4f2f15cde0a74ad7b02
安忆 <i@anyi.in>
2024-08-13 17:59:17 +0800

    refactor(components/Footer): add Alipay donation QR code and link

--- app/(pages)/footer.tsx
+++ app/(pages)/footer.tsx
@@ -3,6 +3,8 @@ import {execSync} from 'node:child_process';
 
 import {Link, type LinkProps, Tooltip} from '@nextui-org/react';
 
+import QRCode from '@/components/qrCode';
+
 import {siteConfig} from '@/configs';
 
 const {links, shortName, version, nodeEnv, vercelEnv} = siteConfig;
@@ -13,8 +15,8 @@ const sha = (
 	(isProduction ? execSync('git rev-parse --short HEAD').toString('utf8') : null)
 )?.trim();
 
-interface IFooterLinkProps extends Pick<LinkProps, 'href' | 'isExternal' | 'showAnchorIcon'> {
-	content: string;
+interface IFooterLinkProps extends Pick<LinkProps, 'href' | 'isExternal' | 'showAnchorIcon' | 'title'> {
+	content?: string;
 }
 
 const FooterLink = memo<PropsWithChildren<IFooterLinkProps>>(function FooterLink({
@@ -22,48 +24,83 @@ const FooterLink = memo<PropsWithChildren<IFooterLinkProps>>(function FooterLink
 	content,
 	isExternal = true,
 	showAnchorIcon = false,
+	title,
 	children,
 }) {
 	return (
-		<Tooltip showArrow content={content}>
-			<Link
-				isExternal={isExternal}
-				showAnchorIcon={showAnchorIcon}
-				href={href}
-				aria-label={content}
-				className="text-xs opacity-60 dark:text-warning-400 dark:opacity-40 dark:hover:opacity-60"
-			>
-				{children}
-			</Link>
-		</Tooltip>
+		<Link
+			isExternal={isExternal}
+			showAnchorIcon={showAnchorIcon}
+			href={href}
+			aria-label={content ?? title ?? (children as string)}
+			title={title}
+			className="text-xs opacity-60 dark:text-warning-400 dark:opacity-40 dark:hover:opacity-60"
+		>
+			{children}
+		</Link>
 	);
 });
+
+interface IFooterLinkWithTooltipProps extends IFooterLinkProps {
+	content: string;
+}
+
+const FooterLinkWithTooltip = memo<PropsWithChildren<IFooterLinkWithTooltipProps>>(
+	function FooterLinkWithTooltip(props) {
+		return (
+			<Tooltip showArrow content={props.content}>
+				<FooterLink {...props} />
+			</Tooltip>
+		);
+	}
+);
 
 export default memo(function Footer() {
 	return (
 		<footer className="mx-auto flex max-w-p-95 flex-col items-center justify-center pb-3 text-center text-xs text-default-300 dark:text-default-400 md:max-w-full">
 			<p>
-				<FooterLink content={links.github.label} href={links.github.href}>
+				<FooterLinkWithTooltip isExternal content={links.github.label} href={links.github.href}>
 					{shortName}
-				</FooterLink>
+				</FooterLinkWithTooltip>
 				内所涉及的公司名称、商标、产品等均为其各自所有者的资产，仅供识别。游戏素材版权均归
-				<FooterLink showAnchorIcon content={links.steam.label} href={links.steam.href}>
+				<FooterLinkWithTooltip isExternal showAnchorIcon content={links.steam.label} href={links.steam.href}>
 					原作者
-				</FooterLink>
+				</FooterLinkWithTooltip>
 				所有
 			</p>
-			<p>
-				当前版本：v{version}-
-				{sha ? (
-					<>
-						{vercelEnv ?? nodeEnv}-
-						<FooterLink content="在GitHub上查看此提交" href={`${links.github.href}/commit/${sha}`}>
-							{sha}
-						</FooterLink>
-					</>
-				) : (
-					<>{isProduction ? '' : nodeEnv}</>
-				)}
+			<p className="[&>*]:after:mx-1 [&>*]:after:content-['|'] last:[&>*]:after:hidden">
+				<span>
+					当前版本：v{version}-
+					{sha ? (
+						<>
+							{vercelEnv ?? nodeEnv}-
+							<FooterLinkWithTooltip
+								isExternal
+								content="在GitHub上查看此提交"
+								href={`${links.github.href}/commit/${sha}`}
+							>
+								{sha}
+							</FooterLinkWithTooltip>
+						</>
+					) : (
+						<>{isProduction ? '' : nodeEnv}</>
+					)}
+				</span>
+				<Tooltip
+					showArrow
+					content={
+						<QRCode alt="支付宝收款码" src="/assets/Alipay.png" className="h-20">
+							支付宝收款码
+						</QRCode>
+					}
+					classNames={{
+						content: 'px-1',
+					}}
+				>
+					<FooterLink isExternal showAnchorIcon href={links.donate.href} title={links.donate.label}>
+						支持{shortName}
+					</FooterLink>
+				</Tooltip>
 			</p>
 		</footer>
 	);
