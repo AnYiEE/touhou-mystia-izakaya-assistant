42f98bc754a231f1883e348eab575ebee27fd2f9
安忆 <i@anyi.in>
2024-08-17 05:32:55 +0800

    refactor(components/H3): add `subTitle` and `subTitleClassName` props

--- app/components/h3.tsx
+++ app/components/h3.tsx
@@ -3,14 +3,26 @@ import {twMerge} from 'tailwind-merge';
 
 interface IProps extends Pick<HTMLAttributes<HTMLHeadingElement>, 'className'> {
 	isFirst?: boolean;
+	subTitle?: ReactNodeWithoutBoolean;
+	subTitleClassName?: HTMLAttributes<HTMLSpanElement>['className'];
 }
 
 export default memo(
-	forwardRef<HTMLHeadingElement | null, PropsWithChildren<IProps>>(function H3({className, isFirst, children}, ref) {
+	forwardRef<HTMLHeadingElement | null, PropsWithChildren<IProps>>(function H3(
+		{className, isFirst, subTitle, subTitleClassName, children},
+		ref
+	) {
 		return (
-			<h3 className={twMerge('mb-3 text-lg font-medium', !isFirst && 'mt-4', className)} ref={ref}>
-				{children}
-			</h3>
+			<>
+				<h3 className={twMerge('mb-3 text-lg font-medium', !isFirst && 'mt-4', className)} ref={ref}>
+					{children}
+				</h3>
+				{subTitle && (
+					<span className={twMerge('-mt-3 mb-3 block text-sm text-foreground-500', subTitleClassName)}>
+						{subTitle}
+					</span>
+				)}
+			</>
 		);
 	})
 );
