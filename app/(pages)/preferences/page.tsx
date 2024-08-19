bd34e8a68a282757fb6b49d2efcb449072cb1f79
安忆 <i@anyi.in>
2024-08-20 00:36:05 +0800

    refactor(pages/preferences): display skeleton before content mount

--- app/(pages)/preferences/page.tsx
+++ app/(pages)/preferences/page.tsx
@@ -1,7 +1,18 @@
+'use client';
+
 import {memo} from 'react';
 
+import {useMounted} from '@/hooks';
+
 import Content from './content';
+import Loading from '@/loading';
 
 export default memo(function Preferences() {
+	const isMounted = useMounted();
+
+	if (!isMounted) {
+		return <Loading />;
+	}
+
 	return <Content />;
 });
