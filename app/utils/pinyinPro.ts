3b4c931c7ecada83a8e9ecaaa148e5baee36ad89
安忆 <i@anyi.in>
2024-09-05 00:42:39 +0800

    chore: update tachie images

--- app/utils/pinyinPro.ts
+++ app/utils/pinyinPro.ts
@@ -1,8 +1,13 @@
+/* eslint-disable sort-keys */
 import {customPinyin} from 'pinyin-pro';
 
 customPinyin(
 	{
+		// cSpell:disable
+		番长服: 'fān zhǎng fú',
+		访问着和服: 'fǎng wèn zhuó hé fú',
 		冴月麟: 'hù yuè lín',
+		// cSpell:enable
 	},
 	{
 		multiple: 'replace',
