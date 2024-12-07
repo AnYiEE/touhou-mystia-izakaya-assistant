db393a521e7ae4d0f6e451ed08990dd2173d68a2
安忆 <i@anyi.in>
2024-12-07 17:26:53 +0800

    refactor(components): replace `HTMLAttributes` types with more specific type aliases

--- app/(pages)/customer-rare/tagGroup.tsx
+++ app/(pages)/customer-rare/tagGroup.tsx
@@ -1,7 +1,7 @@
-import {type HTMLAttributes, type PropsWithChildren, memo} from 'react';
+import {type PropsWithChildren, memo} from 'react';
 import {twMerge} from 'tailwind-merge';
 
-interface IProps extends Pick<HTMLAttributes<HTMLDivElement>, 'className'> {}
+interface IProps extends Pick<HTMLDivElementAttributes, 'className'> {}
 
 export default memo<PropsWithChildren<IProps>>(function TagGroup({children, className}) {
 	return <div className={twMerge('flex flex-wrap gap-2', className)}>{children}</div>;
