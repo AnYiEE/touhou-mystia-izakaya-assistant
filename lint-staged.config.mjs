d8de0ae32e676b4f66d78b53c53d4b8cc83c25dc
安忆 <i@anyi.in>
2025-01-17 23:35:57 +0800

    build: update dependencies

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -8,7 +8,7 @@ import {relative} from 'node:path';
 const buildEslintCommand = (filenames) =>
 	`next lint --fix --file ${filenames.map((f) => relative(process.cwd(), f)).join(' --file ')}`;
 
-/** @type {import('lint-staged').Config} */
+/** @type {import('lint-staged').Configuration} */
 const config = {
 	'!*.{scss,cjs,mjs,js,jsx,ts,tsx}': 'prettier --ignore-unknown --write',
 	'*.{cjs,mjs,js,jsx,ts,tsx}': ['prettier --write', buildEslintCommand],
