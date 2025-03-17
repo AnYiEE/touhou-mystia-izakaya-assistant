3d1663a4f45d0d8d170c7c04ed754788e0d8358d
安忆 <i@anyi.in>
2025-03-17 15:59:23 +0800

    chore: fix case issues

--- app/design/hooks/use-theme/index.ts
+++ app/design/hooks/use-theme/index.ts
@@ -1,5 +1,5 @@
 export {COLOR_MAP, THEME_MAP} from './constants';
-export {default as ThemeScript} from './ThemeScript';
+export {default as ThemeScript} from './themeScript';
 export * from './useTheme';
 
 export type * from './types';
