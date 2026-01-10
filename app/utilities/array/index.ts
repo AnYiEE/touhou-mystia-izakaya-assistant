fdf65170741d7d3a1e83ebcb9609a35eed525114
安忆 <i@anyi.in>
2026-01-10 12:51:23 +0800

    feat(utilities/array): add `sortBy`
    - Introduced a new utility function `sortBy` to sort one array based on the order of another array.

--- app/utilities/array/index.ts
+++ app/utilities/array/index.ts
@@ -3,5 +3,6 @@ export * from './covert';
 export * from './generateRange';
 export * from './intersection';
 export * from './removeLastElement';
+export * from './sortBy';
 export * from './union';
 export * from './without';
