72e81e11a2e5693fb7c16fc4a09d6872f3f60a3e
安忆 <i@anyi.in>
2026-01-28 21:40:29 +0800

    fix(pages/customer-normal): correct router redirection for null customer names
    - Change redirection from '/customer-rare' to '/customer-normal' when current customer name is null.

--- app/(pages)/customer-normal/[[...paths]]/content.tsx
+++ app/(pages)/customer-normal/[[...paths]]/content.tsx
@@ -108,7 +108,7 @@ export default function Content() {
 
 	useEffect(() => {
 		if (currentCustomerName === null) {
-			router.replace('/customer-rare');
+			router.replace('/customer-normal');
 		}
 	}, [currentCustomerName, router]);
 
